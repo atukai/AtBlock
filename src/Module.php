fe8965dc5c49bff823450b812c2cca91c3f33278
Alexander Tukai <tukai@alutech-mc.com>
2016-10-25 13:57:39 +0300

    Move to psr-4

--- src/Module.php
+++ src/Module.php
@@ -3,30 +3,15 @@
 namespace AtBlock;
 
 use AtBlock\View\Helper\Block as BlockViewHelper;
-use AtBlock\Block\Type;
 
 class Module
 {
-    /**
-     * @return array
-     */
-    public function getAutoloaderConfig()
-    {
-        return [
-            'Zend\Loader\StandardAutoloader' => [
-                'namespaces' => [
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ],
-            ],
-        ];
-    }
-
     /**
      * @return mixed
      */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     /**
